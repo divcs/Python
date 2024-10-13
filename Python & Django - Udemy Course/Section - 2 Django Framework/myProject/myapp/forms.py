1497d45cda2e468b9b29db550068ed729f5d54b2
Divya <singhdivya10aug2002@gmail.com>
2024-10-13 10:13:42 +0530

    Working with GET and POST in Forms

---
+++ Python & Django - Udemy Course/Section - 2 Django Framework/myProject/myapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class FeedbackForm(forms.Form):
+    title = forms.CharField(
+        label="title",
+        max_length=50,
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+    )
+    subject = forms.CharField(
+        label="Subject Description",
+        max_length=200,
+        widget=forms.Textarea(attrs={"class": "form-control"}),
+    )
