0c3990cf9c99e67449c1927ae22ed2835be6060b
Divya <singhdivya10aug2002@gmail.com>
2024-10-13 22:03:37 +0530

    Adding instructions & Submitting query

---
+++ Python & Django - Udemy Course/Section - 2 Django Framework/djangoprojects/calculatorapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+def submitquery(request):
+    q = request.GET["query"]
+    return HttpResponse(q)
+
+
+# def submitquery(request):
+#     q = request.GET["query"]
+#     jsondict = {"q": q}
+#     return JsonResponse(jsondict)
