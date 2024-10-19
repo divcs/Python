3adf3bf1e0f3a2264c7b452ef23d3d1c93aa540a
Divya <singhdivya10aug2002@gmail.com>
2024-10-19 16:27:14 +0530

    QR Code Generator

--- Python & Django - Udemy Course/Section - 2 Django Framework/djangoprojects/votingapp/views.py
+++ Python & Django - Udemy Course/Section - 2 Django Framework/djangoprojects/votingapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 
 # Create your views here.
@@ -6,3 +7,8 @@ def index(request):
     arr = ["Java", "Python", "CPlusPlus", "JavaScript", "PHP", "SQL"]
     mydictionary = {"arr": arr}
     return render(request, "index.html", context=mydictionary)
+
+
+def getquery(request):
+    q = request.GET[""]
+    return HttpResponse(q)
